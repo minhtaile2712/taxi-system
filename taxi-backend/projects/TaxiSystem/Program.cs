91118bb6332b834475ed02d1c7b15b35c0389d67
minhtaile2712 <minhtaile2712@gmail.com>
2024-05-29 17:47:27 +0700

    Add API docs configuration

--- taxi-backend/projects/TaxiSystem/Program.cs
+++ taxi-backend/projects/TaxiSystem/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using TaxiSystem.Models;
 using TaxiSystem.Models.Drivers;
 
@@ -13,15 +14,19 @@ builder.Services.AddScoped<IDriversService, DriversService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+  var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+  options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
