b1c39db301c9cd36db93072065f029c92e5013cb
minhtaile2712 <minhtaile2712@gmail.com>
2024-05-29 09:10:23 +0700

    temp

---
+++ abc/TaxiServer/Models/Customers/Customer.cs
@@ -0,0 +1,23 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace TaxiServer.Models.Customers;
+
+public class Customer
+{
+    public string Id { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public Point? Location { get; set; }
+
+    public Customer(string phoneNumber)
+    {
+        Id = phoneNumber;
+        PhoneNumber = phoneNumber;
+    }
+
+    public void SetLocation(Point? location)
+    {
+        Location = location;
+    }
+}
