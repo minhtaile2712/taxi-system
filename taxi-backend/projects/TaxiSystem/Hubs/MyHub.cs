e05add4434c7924d3c00f8c238d90811789e69db
minhtaile2712 <minhtaile2712@gmail.com>
2024-05-30 18:54:45 +0700

    Add realtime function

---
+++ taxi-backend/projects/TaxiSystem/Hubs/MyHub.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TaxiSystem.Hubs;
+
+public class Message
+{
+    public string Content { get; set; } = null!;
+    public string SenderId { get; set; } = null!;
+    public DateTimeOffset SentTime { get; set; }
+}
+
+public class MyHub : Hub
+{
+    private static readonly List<Message> MessageHistory = new();
+
+    public async Task SendMessageToOthers(string content)
+    {
+        var senderId = Context.ConnectionId;
+        DateTimeOffset sentTime = WriteHisory(content, senderId);
+        await Clients.Others.SendAsync("ReceiveMessage", content, senderId, sentTime);
+    }
+
+    public async Task SendMessageToAll(string content)
+    {
+        var senderId = Context.ConnectionId;
+        DateTimeOffset sentTime = WriteHisory(content, senderId);
+        await Clients.All.SendAsync("ReceiveMessage", content, senderId, sentTime);
+    }
+
+    public async Task RetrieveMessageHistory() =>
+        await Clients.Caller.SendAsync("MessageHistory", MessageHistory);
+
+    private DateTimeOffset WriteHisory(string content, string senderId)
+    {
+        var sentTime = DateTimeOffset.UtcNow;
+        MessageHistory.Add(new()
+        {
+            Content = content,
+            SenderId = senderId,
+            SentTime = sentTime
+        });
+        return sentTime;
+    }
+}
--- taxi-backend/projects/TaxiSystem/Program.cs
+++ taxi-backend/projects/TaxiSystem/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using TaxiSystem.Hubs;
 using TaxiSystem.Models;
 using TaxiSystem.Models.Drivers;
 
@@ -11,22 +12,40 @@ builder.Services.AddDbContext<TaxiSystemContext>(options => options
   .UseNpgsql(builder.Configuration.GetConnectionString("TaxiSystem"), o => o.UseNetTopologySuite()));
 builder.Services.AddScoped<IDriversService, DriversService>();
 
+builder.Services.AddSignalR();
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-  var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-  options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder
+                .SetIsOriginAllowed(origin => new Uri(origin).IsLoopback)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
 });
 
 var app = builder.Build();
 
+app.UseCors();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -34,5 +53,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<MyHub>("/hub");
 
 app.Run();
